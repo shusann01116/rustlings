5ba1ba7c72456c36ff7fc412e43f3aebc303d474
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-02-29 14:42:11 +0000

    Till quiz2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+    let cost = if num > 40 { 1 } else { 2 };
+    num * cost
+}
 
 // Don't modify this function!
 #[test]
